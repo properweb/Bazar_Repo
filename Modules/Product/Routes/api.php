5ca548771c77cd49602e1f8df9e50a254958955e
arijit1111 <116731272+arijit1111@users.noreply.github.com>
2022-11-23 16:39:56 +0530

    new

---
+++ Modules/Product/Routes/api.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::prefix('product')->group(function () {
+    Route::get('/', 'ProductController@index');
+});
+Route::prefix('product')->group(function () {
+    Route::post('/create', 'ProductController@create');
+});
+Route::prefix('product')->group(function () {
+    Route::get('/countries', 'ProductController@countries');
+});
+Route::prefix('product')->group(function () {
+    Route::get('/allcategory', 'ProductController@allcategory');
+});
+Route::prefix('product')->group(function () {
+    Route::get('/convertprice/{price}', 'ProductController@convertprice');
+});
+Route::prefix('product')->group(function () {
+    Route::get('/fetchproductbysort', 'ProductController@fetchproductbysort');
+});
+Route::prefix('product')->group(function () {
+    Route::get('/productdetails', 'ProductController@productdetails');
+});
+Route::prefix('product')->group(function () {
+    Route::post('/updateproduct', 'ProductController@updateproduct');
+});
+Route::prefix('product')->group(function () {
+    Route::get('/statusproduct', 'ProductController@statusproduct');
+});
+Route::prefix('product')->group(function () {
+    Route::get('/deleteproduct', 'ProductController@deleteproduct');
+});
+Route::prefix('product')->group(function () {
+    Route::get('/deleteproductimage', 'ProductController@deleteproductimage');
+});
+Route::prefix('product')->group(function () {
+    Route::get('/deleteproductvideo', 'ProductController@deleteproductvideo');
+});
+Route::prefix('product')->group(function () {
+    Route::get('/fetchproductbyvendor', 'ProductController@fetchproductbyvendor');
+});
+Route::prefix('product')->group(function () {
+    Route::post('/productsreorder', 'ProductController@productsreorder');
+});
+Route::prefix('product')->group(function () {
+    Route::GET('importshopify', 'ProductController@ImportShopify');
+});
+Route::prefix('product')->group(function () {
+    Route::GET('fetch-products', 'ProductController@FetchProducts');
+});
+Route::prefix('product')->group(function () {
+    Route::post('/update-products-stock', 'ProductController@UpdateProductsStock');
+});
+Route::prefix('product')->group(function () {
+    Route::GET('/synctoshopify', 'ProductController@SyncToShopify');
+});
