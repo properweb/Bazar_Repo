874215561da7d8c630f8dd42d383a8e52b63ae83
Purbayan <purbayan@properbounce.com>
2022-11-28 18:26:11 +0530

    initial commit

--- Modules/Brand/Http/Controllers/BrandController.php
+++ Modules/Brand/Http/Controllers/BrandController.php
@@ -11,12 +11,20 @@ use Illuminate\Support\Str;
 use Modules\Brand\Entities\Catalog;
 use Modules\Brand\Entities\User;
 use Modules\Brand\Entities\Brand;
+use Modules\Product\Entities\Category;
 use File;
 use Mail;
 use DB;
 
 class BrandController extends Controller
 {
+
+    private $brandUploadPath = "";
+
+    public function __construct()
+    {
+        $this->brandUploadPath = '/uploads/brands/';
+    }
 
     public function index()
     {
@@ -43,7 +51,7 @@ class BrandController extends Controller
                     'brand_key' => $rand_key,
                     'bazaar_direct_link' => $rand_key,
                 ));
-                $vendor = Brand::updateOrCreate(['user_id' => $userId], $request->except(['email', 'password', 'first_name', 'last_name']));
+                Brand::updateOrCreate(['user_id' => $userId], $request->except(['email', 'password', 'first_name', 'last_name']));
                 $data['vendor_id'] = $userId;
                 $data['brand_key'] = $request->brand_key;
                 $data['bazaar_direct_link'] = $request->bazaar_direct_link;
@@ -60,7 +68,8 @@ class BrandController extends Controller
         $data = (array)$request->all();
         $request->bazaar_direct_link = Str::slug($request->bazaar_direct_link, '-');
         $request->brand_slug = Str::slug($request->brand_name, '-');
-        $vendor = Brand::updateOrCreate(['user_id' => request()->user_id], $request->except(['email', 'password', 'first_name', 'last_name', 'featured_image', 'profile_photo', 'cover_image']));
+        $brand = Brand::updateOrCreate(['user_id' => request()->user_id], $request->except(['email', 'password', 'first_name', 'last_name', 'featured_image', 'profile_photo', 'cover_image']));
+        $brand_id = $brand->id;
 
 
         if (isset($request->first_name) && isset($request->last_name)) {
@@ -70,58 +79,33 @@ class BrandController extends Controller
             $user->save();
         }
 
-
-        $vndr_upload_path = '/uploads/brands/' . request()->user_id;
-        if (!file_exists(public_path() . $vndr_upload_path)) {
-            mkdir(public_path() . $vndr_upload_path, 0777, true);
+        $brandUploadPath = $this->brandUploadPath . $brand_id;
+        if (!file_exists(public_path() . $brandUploadPath)) {
+            mkdir(public_path() . $brandUploadPath, 0777, true);
         }
 
         $featured_image = $request->featured_image;
         if (isset($featured_image) && $featured_image != "") {
-            $image_64 = $featured_image; //your base64 encoded data
-            $extension = explode('/', explode(':', substr($image_64, 0, strpos($image_64, ';')))[1])[1];   // .jpg .png .pdf
-            $replace = substr($image_64, 0, strpos($image_64, ',') + 1);
-            $image = str_replace($replace, '', $image_64);
-            $image = str_replace(' ', '+', $image);
-            $image_name = Str::random(10) . '.' . 'png';
-            File::put(public_path() . $vndr_upload_path . "/" . $image_name, base64_decode($image));
-            $vendor->featured_image = $vndr_upload_path . "/" . $image_name;
-            $status = $vendor->save();
+            $brand->featured_image = $this->imageUpload($brand_id, $featured_image, null, false);
+            $status = $brand->save();
         }
-
 
         $profile_photo = $request->profile_photo;
         if (isset($profile_photo) && $profile_photo != "") {
-            $image_64 = $profile_photo; //your base64 encoded data
-            $extension = explode('/', explode(':', substr($image_64, 0, strpos($image_64, ';')))[1])[1];   // .jpg .png .pdf
-            $replace = substr($image_64, 0, strpos($image_64, ',') + 1);
-            $image = str_replace($replace, '', $image_64);
-            $image = str_replace(' ', '+', $image);
-            $image_name = Str::random(10) . '.' . 'png';
-            File::put(public_path() . $vndr_upload_path . "/" . $image_name, base64_decode($image));
-            $vendor->profile_photo = $vndr_upload_path . "/" . $image_name;
-            $status = $vendor->save();
+            $brand->profile_photo = $this->imageUpload($brand_id, $profile_photo, null, false);
+            $status = $brand->save();
         }
 
         $cover_image = $request->cover_image;
         if (isset($cover_image) && $cover_image != "") {
-            $image_64 = $cover_image; //your base64 encoded data
-            $extension = explode('/', explode(':', substr($image_64, 0, strpos($image_64, ';')))[1])[1];   // .jpg .png .pdf
-            $replace = substr($image_64, 0, strpos($image_64, ',') + 1);
-            $image = str_replace($replace, '', $image_64);
-            $image = str_replace(' ', '+', $image);
-            $image_name = Str::random(10) . '.' . 'png';
-            File::put(public_path() . $vndr_upload_path . "/" . $image_name, base64_decode($image));
-            $vendor->cover_image = $vndr_upload_path . "/" . $image_name;
-            $status = $vendor->save();
+            $brand->cover_image = $this->imageUpload($brand_id, $cover_image, null, true);
         }
-
 
         $wholesle_xlsx = $request->upload_wholesale_xlsx;
         if (isset($wholesle_xlsx) && !empty($wholesle_xlsx)) {
             $excel_file_names = $_FILES["upload_wholesale_xlsx"]["name"];
             if (count($excel_file_names) > 0) {
-                $folderPath = public_path() . $vndr_upload_path . "/";
+                $folderPath = public_path() . $brandUploadPath . "/";
                 for ($i = 0; $i < count($excel_file_names); $i++) {
                     $file_name = $excel_file_names[$i];
                     $tmp_arr = explode(".", $file_name);
@@ -129,8 +113,8 @@ class BrandController extends Controller
                     $file_url = Str::random(10) . '_prices.' . $extension;
                     move_uploaded_file($_FILES["upload_wholesale_xlsx"]["tmp_name"][$i], $folderPath . $file_url);
                     $catalog = new Catalog();
-                    $catalog->brand_id = $vendor->id;
-                    $catalog->filename = $vndr_upload_path . "/" . $file_url;
+                    $catalog->brand_id = $brand->id;
+                    $catalog->filename = $brandUploadPath . "/" . $file_url;
                     $catalog->save();
                 }
             }
@@ -139,29 +123,29 @@ class BrandController extends Controller
         $upload_zip = $request->upload_zip;
         if (isset($upload_zip) && $upload_zip != '') {
             $zip_file_name = $_FILES["upload_zip"]["name"];
-            $folderPath = public_path() . $vndr_upload_path . "/";
+            $folderPath = public_path() . $brandUploadPath . "/";
             $file_name = $zip_file_name;
             $tmp_arr = explode(".", $file_name);
             $extension = end($tmp_arr);
             $original_file_name = pathinfo($file_name, PATHINFO_FILENAME);
             $file_url = Str::random(10) . '_photos.' . $extension;
             move_uploaded_file($_FILES["upload_zip"]["tmp_name"], $folderPath . $file_url);
-            $vendor->upload_zip = $vndr_upload_path . "/" . $file_url;
-            $status = $vendor->save();
+            $brand->upload_zip = $brandUploadPath . "/" . $file_url;
+            $status = $brand->save();
         }
 
         $upload_contact_list = $request->upload_contact_list;
         if (isset($upload_contact_list) && $upload_contact_list != '') {
             $csv_file_name = $_FILES["upload_contact_list"]["name"];
-            $folderPath = public_path() . $vndr_upload_path . "/";
+            $folderPath = public_path() . $brandUploadPath . "/";
             $file_name = $csv_file_name;
             $tmp_arr = explode(".", $file_name);
             $extension = end($tmp_arr);
             $original_file_name = pathinfo($file_name, PATHINFO_FILENAME);
             $file_url = Str::random(10) . '_cstmrs.' . $extension;
             move_uploaded_file($_FILES["upload_contact_list"]["tmp_name"], $folderPath . $file_url);
-            $vendor->upload_contact_list = $vndr_upload_path . "/" . $file_url;
-            $status = $vendor->save();
+            $brand->upload_contact_list = $brandUploadPath . "/" . $file_url;
+            $status = $brand->save();
         }
 
         $response = ['res' => true, 'msg' => "", 'data' => $data];
@@ -213,7 +197,7 @@ class BrandController extends Controller
             $response = ['res' => false, 'msg' => $validator->errors()->first(), 'data' => ""];
         } else {
 
-            $vendor = Brand::updateOrCreate(['user_id' => request()->user_id], $request->except(['email', 'featured_image', 'profile_photo', 'cover_image', 'logo_image']));
+            $brand = Brand::updateOrCreate(['user_id' => request()->user_id], $request->except(['email', 'featured_image', 'profile_photo', 'cover_image', 'logo_image']));
             if (isset($request->email)) {
                 $user = User::find($user_id);
                 $user->email = $request->email;
@@ -221,96 +205,26 @@ class BrandController extends Controller
             }
             $profile_photo = $request->profile_photo;
             if (isset($profile_photo) && $profile_photo != "") {
-
-                $temp_string = '/uploads/brands/' . request()->user_id;
-                if (!file_exists(public_path() . $temp_string)) {
-                    mkdir(public_path() . $temp_string, 0777, true);
-                }
-                if ($vendor->profile_photo != "") {
-                    $unlink_url = public_path() . $temp_string . '/' . $vendor->profile_photo;
-                    if (file_exists($unlink_url)) {
-                        unlink($unlink_url);
-                    }
-                }
-
-                $image_64 = $profile_photo; //your base64 encoded data
-                $extension = explode('/', explode(':', substr($image_64, 0, strpos($image_64, ';')))[1])[1];   // .jpg .png .pdf
-                $replace = substr($image_64, 0, strpos($image_64, ',') + 1);
-                $image = str_replace($replace, '', $image_64);
-                $image = str_replace(' ', '+', $image);
-                $image_name = Str::random(10) . '.' . 'png';
-                File::put(public_path() . $temp_string . "/" . $image_name, base64_decode($image));
-                $vendor->profile_photo = $temp_string . "/" . $image_name;
+                $brand->profile_photo = $this->imageUpload($brand_id, $profile_photo, $brand->profile_photo, true);
             }
 
             $cover_image = $request->cover_image;
             if (isset($cover_image) && $cover_image != "") {
-                $temp_string = '/uploads/brands/' . request()->user_id;
-                if (!file_exists(public_path() . $temp_string)) {
-                    mkdir(public_path() . $temp_string, 0777, true);
-                }
-                if ($vendor->cover_image != "") {
-                    $unlink_url = public_path() . $temp_string . '/' . $vendor->cover_image;
-                    if (file_exists($unlink_url)) {
-                        unlink($unlink_url);
-                    }
-                }
-                $image_64 = $cover_image; //your base64 encoded data
-                $extension = explode('/', explode(':', substr($image_64, 0, strpos($image_64, ';')))[1])[1];   // .jpg .png .pdf
-                $replace = substr($image_64, 0, strpos($image_64, ',') + 1);
-                $image = str_replace($replace, '', $image_64);
-                $image = str_replace(' ', '+', $image);
-                $image_name = Str::random(10) . '.' . 'png';
-                File::put(public_path() . $temp_string . "/" . $image_name, base64_decode($image));
-                $vendor->cover_image = $temp_string . "/" . $image_name;
+                $brand->cover_image = $this->imageUpload($brand_id, $cover_image, $brand->cover_image, true);
             }
 
             $featured_image = $request->featured_image;
             if (isset($featured_image) && $featured_image != "") {
-                $temp_string = '/uploads/brands/' . request()->user_id;
-                if (!file_exists(public_path() . $temp_string)) {
-                    mkdir(public_path() . $temp_string, 0777, true);
-                }
-                if ($vendor->featured_image != "") {
-                    $unlink_url = public_path() . $temp_string . '/' . $vendor->featured_image;
-                    if (file_exists($unlink_url)) {
-                        unlink($unlink_url);
-                    }
-                }
-                $image_64 = $featured_image; //your base64 encoded data
-                $extension = explode('/', explode(':', substr($image_64, 0, strpos($image_64, ';')))[1])[1];   // .jpg .png .pdf
-                $replace = substr($image_64, 0, strpos($image_64, ',') + 1);
-                $image = str_replace($replace, '', $image_64);
-                $image = str_replace(' ', '+', $image);
-                $image_name = Str::random(10) . '.' . 'png';
-                File::put(public_path() . $temp_string . "/" . $image_name, base64_decode($image));
-                $vendor->featured_image = $temp_string . "/" . $image_name;
+                $brand->featured_image = $this->imageUpload($brand_id, $featured_image, $brand->featured_image, true);
             }
 
             $logo_image = $request->logo_image;
             if (isset($logo_image) && $logo_image != "") {
-                $temp_string = '/uploads/brands/' . request()->user_id;
-                if (!file_exists(public_path() . $temp_string)) {
-                    mkdir(public_path() . $temp_string, 0777, true);
-                }
-                if ($vendor->logo_image != "") {
-                    $unlink_url = public_path() . $temp_string . '/' . $vendor->logo_image;
-                    if (file_exists($unlink_url)) {
-                        unlink($unlink_url);
-                    }
-                }
-                $image_64 = $logo_image; //your base64 encoded data
-                $extension = explode('/', explode(':', substr($image_64, 0, strpos($image_64, ';')))[1])[1];   // .jpg .png .pdf
-                $replace = substr($image_64, 0, strpos($image_64, ',') + 1);
-                $image = str_replace($replace, '', $image_64);
-                $image = str_replace(' ', '+', $image);
-                $image_name = Str::random(10) . '.' . 'png';
-                File::put(public_path() . $temp_string . "/" . $image_name, base64_decode($image));
-                $vendor->logo_image = $temp_string . "/" . $image_name;
+                $brand->logo_image = $this->imageUpload($brand_id, $logo_image, $brand->logo_image, true);
             }
 
-            $vendor->first_visit = '1';
-            $status = $vendor->save();
+            $brand->first_visit = '1';
+            $status = $brand->save();
             if ($status) {
                 $response = ['res' => true, 'msg' => "Successfully updated your account", 'data' => ''];
             } else {
@@ -321,5 +235,55 @@ class BrandController extends Controller
         return response()->json($response);
     }
 
+    /**
+     * Update the specified resource in storage.
+     * @param int $brand
+     * @param int $id
+     */
+    private function imageUpload($brand, $image, $previousFile, $replaceable)
+    {
+
+        $brandUploadPath = $this->brandUploadPath . $brand;
+        if (!file_exists(public_path() . $brandUploadPath)) {
+            mkdir(public_path() . $brandUploadPath, 0777, true);
+        }
+
+        if ($replaceable && $previousFile !== null) {
+            $unlinkUrl = public_path() . $previousFile;
+            if (file_exists($unlinkUrl)) {
+                unlink($unlinkUrl);
+            }
+        }
+
+        $image_64 = $image; //your base64 encoded data
+        $extension = explode('/', explode(':', substr($image_64, 0, strpos($image_64, ';')))[1])[1];   // .jpg .png .pdf
+        $replace = substr($image_64, 0, strpos($image_64, ',') + 1);
+        $image_64 = str_replace($replace, '', $image_64);
+        $image_64 = str_replace(' ', '+', $image_64);
+        $imageName = Str::random(10) . '.' . 'png';
+        File::put(public_path() . $brandUploadPath . "/" . $imageName, base64_decode($image_64));
+        return $brandUploadPath . "/" . $imageName;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * @param int $id
+     * @return Renderable
+     */
+    public function category()
+    {
+        $categories = Category::where('parent_id', '0')->where('status', '0')->orderBy('name', 'ASC')->get();
+        $allcategory = array();
+        if (count($categories) > 0) {
+            foreach ($categories as $category) {
+                $allcategory[] = array(
+                    'category' => $category->name,
+                    'cat_id' => $category->id
+                );
+            }
+        }
+        $response = ['res' => true, 'msg' => "", 'data' => $allcategory];
+        return response()->json($response);
+    }
 
 }
