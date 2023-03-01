4d659b0d2ec48a6898ea34324d697957404aca78
Manish <manish@properbounce.com>
2023-03-01 18:39:54 +0530

    cleaning repo

--- Modules/Wordpress/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('wordpress::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('wordpress.name') !!}
-    </p>
-@endsection
