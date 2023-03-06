0199f28e1306326bbe474116c3fcbb7e00f39dda
arijit1111 <116731272+arijit1111@users.noreply.github.com>
2023-03-06 11:07:13 +0530

    Update 2023_03_02_085257_create_product_variations_table.php

--- Modules/Product/Database/Migrations/2023_03_02_085257_create_product_variations_table.php
+++ Modules/Product/Database/Migrations/2023_03_02_085257_create_product_variations_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('variant_key')->nullable();
             $table->string('image')->nullable();
             $table->string('swatch_image')->nullable();
+            $table->unsignedBigInteger('product_id')->nullable();
             $table->foreign('product_id')->references('id')->on('products')->onDelete('CASCADE');
             $table->float('price')->nullable();
             $table->string('options1')->nullable();
