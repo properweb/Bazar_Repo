e9a47e98b8dc4165604eeb4659860251ecbcf74a
arijit1111 <116731272+arijit1111@users.noreply.github.com>
2023-03-06 11:19:24 +0530

    Update 2023_03_02_085257_create_product_variations_table.php

--- Modules/Product/Database/Migrations/2023_03_02_085257_create_product_variations_table.php
+++ Modules/Product/Database/Migrations/2023_03_02_085257_create_product_variations_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('product_variations', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('variant_key')->nullable();
             $table->string('image')->nullable();
             $table->string('swatch_image')->nullable();
