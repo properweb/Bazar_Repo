9f601c462af181e3b012ca743e9d6d85881693e7
arijit1111 <116731272+arijit1111@users.noreply.github.com>
2023-03-06 11:05:34 +0530

    Update 2023_03_02_084311_create_product_images_table.php

--- Modules/Product/Database/Migrations/2023_03_02_084311_create_product_images_table.php
+++ Modules/Product/Database/Migrations/2023_03_02_084311_create_product_images_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('product_images', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedBigInteger('product_id')->nullable();
             $table->foreign('product_id')->references('id')->on('products')->onDelete('CASCADE');
             $table->string('images')->nullable();
             $table->integer('image_id')->nullable();
