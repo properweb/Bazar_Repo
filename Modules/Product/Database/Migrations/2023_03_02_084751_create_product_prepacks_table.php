d5d1ea4c9a3e946727a08ae8e76a8e42f20f1c8d
arijit1111 <116731272+arijit1111@users.noreply.github.com>
2023-03-06 11:06:20 +0530

    Update 2023_03_02_084751_create_product_prepacks_table.php

--- Modules/Product/Database/Migrations/2023_03_02_084751_create_product_prepacks_table.php
+++ Modules/Product/Database/Migrations/2023_03_02_084751_create_product_prepacks_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('product_prepacks', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedBigInteger('product_id')->nullable();
             $table->foreign('product_id')->references('id')->on('products')->onDelete('CASCADE');
             $table->string('style')->charset('utf8');;
             $table->string('pack_name')->charset('utf8');
