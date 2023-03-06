840fd2d980580e1a27425c2495493cbf961eb3b1
arijit1111 <116731272+arijit1111@users.noreply.github.com>
2023-03-06 11:16:37 +0530

    Update 2023_03_02_094926_create_product_video_table.php

--- Modules/Product/Database/Migrations/2023_03_02_094926_create_product_video_table.php
+++ Modules/Product/Database/Migrations/2023_03_02_094926_create_product_video_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('product_video', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('product_id')->nullable();
             $table->foreign('product_id')->references('id')->on('products')->onDelete('CASCADE');
             $table->mediumText('video_url')->nullable();
             $table->timestamps();
