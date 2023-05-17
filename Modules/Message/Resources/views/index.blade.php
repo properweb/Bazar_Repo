dc2b4078774751ce19a6bd1972d01e014f80846f
arijit1111 <116731272+arijit1111@users.noreply.github.com>
2023-05-17 20:17:19 +0530

    messages

--- Modules/Message/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('message::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('message.name') !!}
-    </p>
-@endsection
