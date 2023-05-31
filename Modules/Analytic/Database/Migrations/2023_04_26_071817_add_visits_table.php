adfbde5a7e4f451e5f69c4b748ed3d96b831df5b
arijit1111 <116731272+arijit1111@users.noreply.github.com>
2023-05-31 13:32:31 +0530

    data analytics with missing files

---
+++ Modules/Analytic/Database/Migrations/2023_04_26_071817_add_visits_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('visits', function (Blueprint $table) {
+            $table->id();
+            $table->string('ip_address')->nullable();
+            $table->unsignedBigInteger('brand_id')->nullable();
+            $table->foreign('brand_id')->references('id')->on('users')->onDelete('CASCADE');
+            $table->integer('order')->default('0');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visits');
+    }
+};
